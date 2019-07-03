026254f6287423a4073798ca417e37c05fa68bf1
Daniel Kostro <kostro.d@gmail.com>
2019-07-03 16:32:32 +0200

    additional parse tests

--- src/__tests__/util.js
+++ src/__tests__/util.js
@@ -2,7 +2,7 @@
 
 const util = require('../util');
 
-const example = `
+const german = `
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
 <HTML>
 <HEAD><TITLE>vad-zebra1-la - BEREIT</TITLE><meta http-equiv="Pragma" content="no-cache"><meta http-equiv="Expires" content="0"></HEAD>
@@ -34,10 +34,90 @@ Support: <A HREF="https://www.zebra.com/support.html">https://www.zebra.com/supp
 </HTML>
 `;
 
-test('parse printer result', () => {
-  const parsed = util.parsePrinterResult(example);
+const english = `
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<HTML>
+<HEAD><TITLE>ACI-PRT10 - READY</TITLE><meta http-equiv="Pragma" content="no-cache"><meta http-equiv="Expires" content="0"></HEAD>
+<BODY><CENTER>
+<IMG SRC="logo.png" ALT="[Logo]">
+<H1>Zebra Technologies<BR>
+ZTC ZD420-300dpi ZPL</H1>
+<H2>ACI-PRT10</H2>
+Internal Wired PrintServer<H3>Status: <FONT COLOR="GREEN">READY</FONT></H3>
+<H3><FONT COLOR=RED></FONT></H3>
+<HR>
+<DIV ALIGN="center">
+<H2>Printer Home Page</H2>
+<FORM METHOD="POST" ACTION="index.html">
+<DIV ALIGN="center">
+<H3><A HREF="config.html">View Printer Configuration</A><BR>
+<A HREF="settings">View and Modify Printer Settings</A><BR>
+<A HREF="dir">Directory Listing</A><BR>
+<A HREF="uns">Alert Setup</A><BR>
+<A HREF="control">Printer Controls</A><BR>
+<A HREF="/server/">Print Server Settings</A></H3>
+</CENTER>
+</FORM></DIV>
+<CENTER><HR>
+Home: <A HREF="https://www.zebra.com">https://www.zebra.com</A><BR>
+Support: <A HREF="https://www.zebra.com/support.html">https://www.zebra.com/support.html</A><BR>
+</CENTER>
+</BODY>
+</HTML>
+`;
+
+const englishError = `
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<HTML>
+<HEAD><TITLE>ACI-PRT10 - READY</TITLE><meta http-equiv="Pragma" content="no-cache"><meta http-equiv="Expires" content="0"></HEAD>
+<BODY><CENTER>
+<IMG SRC="logo.png" ALT="[Logo]">
+<H1>Zebra Technologies<BR>
+ZTC ZD420-300dpi ZPL</H1>
+<H2>ACI-PRT10</H2>
+Internal Wired PrintServer<H3>Status: <FONT COLOR="GREEN">ERROR</FONT></H3>
+<H3><FONT COLOR=RED></FONT></H3>
+<HR>
+<DIV ALIGN="center">
+<H2>Printer Home Page</H2>
+<FORM METHOD="POST" ACTION="index.html">
+<DIV ALIGN="center">
+<H3><A HREF="config.html">View Printer Configuration</A><BR>
+<A HREF="settings">View and Modify Printer Settings</A><BR>
+<A HREF="dir">Directory Listing</A><BR>
+<A HREF="uns">Alert Setup</A><BR>
+<A HREF="control">Printer Controls</A><BR>
+<A HREF="/server/">Print Server Settings</A></H3>
+</CENTER>
+</FORM></DIV>
+<CENTER><HR>
+Home: <A HREF="https://www.zebra.com">https://www.zebra.com</A><BR>
+Support: <A HREF="https://www.zebra.com/support.html">https://www.zebra.com/support.html</A><BR>
+</CENTER>
+</BODY>
+</HTML>
+`;
+
+test('parse printer result (german)', () => {
+  const parsed = util.parsePrinterResult(german);
   expect(parsed).toMatchObject({
     isOnline: true,
     serialNumber: 'vad-zebra1-la'
+  });
+});
+
+test('parse printer result (english)', () => {
+  const parsed = util.parsePrinterResult(english);
+  expect(parsed).toMatchObject({
+    isOnline: true,
+    serialNumber: 'ACI-PRT10'
+  });
+});
+
+test('parse printer result not ready', () => {
+  const parsed = util.parsePrinterResult(englishError);
+  expect(parsed).toMatchObject({
+    isOnline: false,
+    serialNumber: 'ACI-PRT10'
   });
 });
