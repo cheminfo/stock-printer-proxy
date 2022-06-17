f2a5fdda04454c70ac5c7d1a440f952d6b8fbc4d
Daniel Kostro <kostro.d@gmail.com>
2022-06-17 11:36:08 +0200

    feat: handle sigterm gracefully

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,29 @@
+import fastifyGracefulShutdown from 'fastify-graceful-shutdown';
+
 import constants from './constants';
 import fastify from './fastify';
 import { startMonitoring } from './monitorZebra';
+
+void fastify.register(fastifyGracefulShutdown);
 
 fastify.log.info({
     ...constants,
     accessToken: '***',
 });
 
+fastify.after(() => {
+    fastify.gracefulShutdown((_, next) => {
+        next();
+    });
+});
+
 // Run the server!
 const start = async () => {
     try {
-        await fastify.listen(
-            constants.port,
-            process.env.NODE_ENV === 'production' ? '0.0.0.0' : undefined,
-        );
+        await fastify.listen({
+            port: constants.port,
+            host: process.env.NODE_ENV === 'production' ? '0.0.0.0' : undefined,
+        });
         fastify.log.info(`listening on port ${constants.port}`);
     } catch (err) {
         fastify.log.error(err);
